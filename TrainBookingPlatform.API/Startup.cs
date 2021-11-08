d50306ec68ddca202a7bf381fe383ca8579b2099
rareseanu <moiseanurares@gmail.com>
2021-11-08 16:53:22 +0200

    Added user service.

--- TrainBookingPlatform.API/Startup.cs
+++ TrainBookingPlatform.API/Startup.cs
@@ -29,6 +29,8 @@ namespace TrainBookingPlatform.API
             services.AddControllers();
             services.AddScoped<ITrainService,TrainService>();
             services.AddScoped<ITrainRepository,TrainRepository>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TrainBookingPlatform.API", Version = "v1" });
---
+++ TrainBookingPlatform.BL/Classes/UserService.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrainBookingPlatform.BL.Interfaces;
+using TrainBookingPlatform.DAL.Entities;
+using TrainBookingPlatform.DAL.Repository.Interfaces;
+
+namespace TrainBookingPlatform.BL.Classes
+{
+    public class UserService : IUserService
+    {
+        private IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<User> Add(User user)
+        {
+            return await _userRepository.Create(user);
+        }
+        public User Delete(int id)
+        {
+            User user = _userRepository.Get(p => p.Id == id).FirstOrDefault();
+            if (user != null)
+            {
+                return _userRepository.Delete(user);
+            }
+            return null;
+        }
+        public User Update(User user)
+        {
+            return _userRepository.Update(user);
+        }
+
+        public User Get(int id)
+        {
+            return _userRepository.Get(p => p.Id == id).FirstOrDefault();
+        }
+        public IEnumerable<User> GetAll()
+        {
+            return _userRepository.GetAll();
+        }
+    }
+}
