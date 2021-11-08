9352bc42bc0b09412f9026cbb9f66e6274668cda
lungudragos22 <lungudragos22@gmail.com>
2021-11-08 18:23:12 +0200

    Created ticket repository and repository interface

---
+++ TrainBookingPlatform.DAL/Repository/Interfaces/ITicketRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainBookingPlatform.DAL.Entities;
+
+namespace TrainBookingPlatform.DAL.Repository.Interfaces
+{
+    public interface ITicketRepository : IBaseRepository<Ticket>
+    {
+
+    }
+}
