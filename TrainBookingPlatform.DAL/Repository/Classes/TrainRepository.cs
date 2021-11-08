af59fbcc035653041071ca9682930d289da5e47b
lungudragos22 <lungudragos22@gmail.com>
2021-11-08 18:19:24 +0200

    Created train repository

---
+++ TrainBookingPlatform.DAL/Repository/Classes/TrainRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainBookingPlatform.DAL.Entities;
+
+namespace TrainBookingPlatform.DAL.Repository.Classes
+{
+    public class TrainRepository : BaseRepository<Train>
+    {
+        public TrainRepository(TrainBookingPlatformDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
