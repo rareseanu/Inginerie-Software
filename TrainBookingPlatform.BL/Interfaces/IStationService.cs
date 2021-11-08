4360d1272e9cf258965cb6b2ca6796a40588e237
George <64893104+geckoro@users.noreply.github.com>
2021-11-08 19:18:13 +0200

    Added station service & repository

---
+++ TrainBookingPlatform.BL/Interfaces/IStationService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TrainBookingPlatform.DAL.Entities;
+
+namespace TrainBookingPlatform.BL.Interfaces
+{
+    public interface IStationService
+    {
+        public Task<Station> Add(Station station);
+        public Station Delete(int id);
+        public Station Update(Station station);
+        public Station Get(int id);
+        public IEnumerable<Station> GetAll();
+    }
+}
---
+++ TrainBookingPlatform.DAL/Repository/Classes/StationRepository.cs
@@ -0,0 +1,13 @@
+﻿using TrainBookingPlatform.DAL.Entities;
+using TrainBookingPlatform.DAL.Repository.Interfaces;
+
+namespace TrainBookingPlatform.DAL.Repository.Classes
+{
+    public class StationRepository : BaseRepository<Station>, IStationRepository
+    {
+        public StationRepository(TrainBookingPlatformDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
