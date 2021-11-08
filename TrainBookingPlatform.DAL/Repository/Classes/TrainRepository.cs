87d3a9d745c43d221c64c2cb7569097c41fdfdf5
lungudragos22 <lungudragos22@gmail.com>
2021-11-08 18:24:30 +0200

    Created train interface

--- TrainBookingPlatform.DAL/Repository/Classes/TrainRepository.cs
+++ TrainBookingPlatform.DAL/Repository/Classes/TrainRepository.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrainBookingPlatform.DAL.Entities;
+using TrainBookingPlatform.DAL.Repository.Interfaces;
 
 namespace TrainBookingPlatform.DAL.Repository.Classes
 {
-    public class TrainRepository : BaseRepository<Train>
+    public class TrainRepository : BaseRepository<Train> , ITrainRepository
     {
         public TrainRepository(TrainBookingPlatformDbContext dbContext) : base(dbContext)
         {
