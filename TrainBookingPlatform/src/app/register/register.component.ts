1a4e35bc862e9f7a73ce346df42e352d06452ae1
George <64893104+geckoro@users.noreply.github.com>
2021-11-09 20:05:01 +0200

    Changes to user management component

--- TrainBookingPlatform/src/app/register/register.component.ts
+++ TrainBookingPlatform/src/app/register/register.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Credentials } from '../credentials';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  credentials: Credentials = {
+    email: "",
+    password: ""
+  }
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
   }
 
+
+  onRegister(): void {
+    console.log(this.credentials);
+    var postData = {
+      email: '',
+      password: 'email@email.com'
+    };
+
+    const headers: HttpHeaders = new HttpHeaders();
+    headers.set('Content-Type', 'application/x-www-form-urlencoded');
+
+    this.http.post('/UserRole/SaveUserRoles', postData, { headers: headers })
+      .subscribe(result => {
+      });
+  }
 }
