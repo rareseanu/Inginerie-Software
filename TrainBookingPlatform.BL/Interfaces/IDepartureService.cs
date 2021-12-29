a674846f67dd7174c716745a510693da66959b52
George <64893104+geckoro@users.noreply.github.com>
2021-12-29 13:19:13 +0200

    Departures have times based on hours and minutes

--- TrainBookingPlatform.BL/Interfaces/IDepartureService.cs
+++ TrainBookingPlatform.BL/Interfaces/IDepartureService.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using TrainBookingPlatform.DAL.Entities;
+using TrainBookingPlatform.TL.DTOs;
 
 namespace TrainBookingPlatform.BL.Interfaces
 {
     public interface IDepartureService
     {
-        public Task<Departure> Add(Departure departure);
+        public Task<Departure> Add(DepartureDTO departureDTO);
         public Task<Departure> Delete(int id);
-        public Task<Departure> Update(Departure departure);
+        public Task<Departure> Update(DepartureDTO departureDTO);
         public Task<Departure> Get(int id);
         public Task<IEnumerable<Departure>> GetAll();
     }
---
+++ TrainBookingPlatform.DAL/Migrations/20211229102657_timespan.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrainBookingPlatform.DAL.Migrations
+{
+    public partial class timespan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "DepartureTime",
+                table: "Departures",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "ArrivalTime",
+                table: "Departures",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DepartureTime",
+                table: "Departures",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ArrivalTime",
+                table: "Departures",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+        }
+    }
+}
